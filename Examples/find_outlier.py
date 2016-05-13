dbbec5f8176c9d50f2265960ad271c5f046f1359
Ajit Kshirsagar <ajitnhacks@gmail.com>
2016-05-13 18:44:25 +0530

    find_outlier and isPrime programs added.

---
+++ Examples/find_outlier.py
@@ -0,0 +1,32 @@
+def find_outlier(integers):
+    if integers[0] % 2 == 0 and integers[1] % 2 == 0:
+        for i in range(2, len(integers)):
+            if integers[i] % 2 != 0:
+                return integers[i]
+    elif integers[0] % 2 != 0 and integers[1] % 2 != 0:
+        if integers[i] % 2 == 0:
+            return integers[i]
+    elif integers[0] % 2 == 0 and integers[1] % 2 != 0 and integers[2] % 2 == 0:
+        return integers[1]
+    elif integers[0] % 2 != 0 and integers[1] % 2 == 0 and integers[2] % 2 != 0:
+        return integers[1]
+    elif integers[0] % 2 != 0 and integers[1] % 2 == 0 and integers[1] % 2 == 0:
+        return integers[0]
+    elif integers[0] % 2 == 0 and integers[1] % 2 != 0 and integers[1] % 2 != 0:
+        return integers[0]
+    else:
+        return 0
+
+
+def find_outlier_expr(integers):
+    odds = [x for x in integers if x % 2 != 0]
+    evens = [x for x in integers if x % 2 == 0]
+
+    return odds[0] if len(odds) < len(evens) else evens[0]
+
+
+def find_outlier_lambda(integers):
+    odds = filter(lambda x: x % 2 != 0, integers)
+    evens = filter(lambda x: x % 2 == 0, integers)
+
+    return odds[0] if len(odds) < len(evens) else evens[0]
